01636f4d59be393cc7543a2af503ebaca5ded0a1
SadesTorm <48841190+SadesTorm@users.noreply.github.com>
2019-07-18 23:29:57 -0300

    Funcao BarCode
    testando leitor de codigos de barra e QR code

--- app/src/main/java/com/sadestorm/tdm/activity/MainActivity.java
+++ app/src/main/java/com/sadestorm/tdm/activity/MainActivity.java
@@ -1,16 +1,21 @@
 package com.sadestorm.tdm.activity;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.sadestorm.tdm.R;
 
 public class MainActivity extends AppCompatActivity {
     Button osBtn;
     Button visitasDiariasBtn;
+    Button scanCodeBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         osBtn = findViewById(R.id.btnOs);
         visitasDiariasBtn = findViewById(R.id.btnVisitasDiarias);
+        scanCodeBtn = findViewById(R.id.btnScanCode);
 
         osBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +40,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        scanCodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scaneando Toner");
+                integrator.setCameraId(0);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null){
+                if(result.getContents() != null ){
+                    Toast.makeText(this,result.getContents() , Toast.LENGTH_SHORT).show();
+                }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
 
 
     }
--- app/src/main/java/com/sadestorm/tdm/activity/RecycleRelatorioDiario.java
+++ app/src/main/java/com/sadestorm/tdm/activity/RecycleRelatorioDiario.java
@@ -1,12 +1,19 @@
 package com.sadestorm.tdm.activity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -17,6 +24,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.sadestorm.tdm.R;
 import com.sadestorm.tdm.adaptadores.AdapterRelatorioDiario;
 import com.sadestorm.tdm.auxiliares.CadastroOs;
+import com.sadestorm.tdm.auxiliares.RecyclerItemClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +36,9 @@ public class RecycleRelatorioDiario extends AppCompatActivity {
     private List<CadastroOs> listaRelatorio;
 
     private AdapterRelatorioDiario adapterRelatorioDiario;
+
+    private String numeroSerie;
+    private String pesquisa;
 
     public DatabaseReference referencia = FirebaseDatabase.getInstance().getReference("OrdemDeServico");
 
@@ -46,15 +57,45 @@ public class RecycleRelatorioDiario extends AppCompatActivity {
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
         adapterRelatorioDiario = new AdapterRelatorioDiario(listaRelatorio);
         recyclerView.setAdapter(adapterRelatorioDiario);
+
+
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), recyclerView,
+                new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
+
+                        CadastroOs aux = listaRelatorio.get(position);
+
+                        Toast.makeText(getApplicationContext(), "Item precionado" + aux.getnSerie(), Toast.LENGTH_LONG).show();
+                        carregaNumSerie(aux.nSerie);
+
+                    }
+
+                    @Override
+                    public void onLongItemClick(View view, int position) {
+                        Toast.makeText(getApplicationContext(), "+-+-+-+-+-- TUDO OK +--+-+-", Toast.LENGTH_LONG).show();
+                        CadastroOs aux = listaRelatorio.get(position);
+
+
+                    }
+
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    }
+                }) {
+        });
+
         carregaDados();
 
     }
 
     public void carregaDados() {
 
+
         Query consulta = referencia.orderByChild("OrdemDeServico");
 
-        consulta.addListenerForSingleValueEvent(new ValueEventListener(){
+        consulta.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 listaRelatorio.clear();
@@ -62,13 +103,44 @@ public class RecycleRelatorioDiario extends AppCompatActivity {
 
                     CadastroOs os = dt.getValue(CadastroOs.class);
                     listaRelatorio.add(os);
+
                 }
                 adapterRelatorioDiario.notifyDataSetChanged();
             }
-                @Override
-                public void onCancelled (@NonNull DatabaseError databaseError){
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void carregaNumSerie(String numeroSerie) {
+
+        final String numSerie = numeroSerie;
+        final String query = "nSerie";
+
+        Query consulta = referencia.orderByChild(query).equalTo(numSerie);
+
+        consulta.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaRelatorio.clear();
+                for (DataSnapshot dt : dataSnapshot.getChildren()) {
+                    if (dt.child(query).getValue() != null) {
+                        CadastroOs os = dt.getValue(CadastroOs.class);
+                        listaRelatorio.add(os);
+                    }
                 }
+
+                adapterRelatorioDiario.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
         });
     }
 }
